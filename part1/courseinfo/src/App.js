6894dbe83be2783336d892ebf0bd966572f5b873
Chelsea Saunders <c_chilcoat@msn.com>
2022-11-08 12:52:26 -0800

    finished 1c

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,58 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
-  const course = "Half Stack application development";
-  const partsArr = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7
-    },
-    {
-      name: "State of a component",
-      exercises: 14
-    }
-  ];
+  const [counter, setCounter] = useState(0);
 
-  const Header = (props) => {
-    return (<h1>{props.course}</h1>);
-  };
+  const increaseCounter = () => setCounter(counter + 1);
+  const decreaseCounter = () => setCounter(counter - 1);
+  const resetCounter = () => setCounter(0);
 
-  const Part = (props) => {
-    return (
-      <p>{props.part} {props.exercises}</p>
-    );
-  };
+  const Display = ({ counter }) => <div>{counter}</div>;
 
-  const Content = (props) => {
-    return (
-      <div>
-        <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
-        <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
-        <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
-      </div>
-    );
-  };
-
-  const Total = (props) => {
-    return (
-      <p>Number of exercises: {
-        props.parts[0].exercises +
-        props.parts[1].exercises +
-        props.parts[2].exercises
-      }</p>
-    );
-  };
+  const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
 
   return (
-    <div>
-      <Header course={course} />
-      <Content parts={partsArr} />
-      <Total parts={partsArr} />
-    </div>
+    <React.Fragment>
+      <Display counter={counter} />
+      <Button onClick={increaseCounter} text="Add 1 to Counter" />
+      <Button onClick={decreaseCounter} text="Subtract 1 from Counter" />
+      <Button onClick={resetCounter} text="Reset Counter" />
+    </React.Fragment>
   );
 };
 
