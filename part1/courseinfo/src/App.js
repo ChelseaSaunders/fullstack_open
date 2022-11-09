a04c8960e5fac3b7dcb4ce028fb8bdbfd24f14f4
Chelsea Saunders <c_chilcoat@msn.com>
2022-11-08 20:55:22 -0800

    working through follow-alongs in lesson 1d

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,24 +1,44 @@
 import React, { useState } from 'react';
 
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (<div>Press the buttons to display click history</div>)
+  }
+
+  return (<div>Click history: {props.allClicks.join(' ')}</div>);
+};
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+);
+
 const App = () => {
-  const [counter, setCounter] = useState(0);
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
 
-  const increaseCounter = () => setCounter(counter + 1);
-  const decreaseCounter = () => setCounter(counter - 1);
-  const resetCounter = () => setCounter(0);
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'));
+    setLeft(left + 1);
+  };
 
-  const Display = ({ counter }) => <div>{counter}</div>;
-
-  const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'));
+    setRight(right + 1);
+  };
 
   return (
-    <React.Fragment>
-      <Display counter={counter} />
-      <Button onClick={increaseCounter} text="Add 1 to Counter" />
-      <Button onClick={decreaseCounter} text="Subtract 1 from Counter" />
-      <Button onClick={resetCounter} text="Reset Counter" />
-    </React.Fragment>
-  );
+    <div>
+      {left}
+      <Button onClick={handleLeftClick} text="Left" />
+      <Button onClick={handleRightClick} text="Right" />
+      {right}
+      <History allClicks={allClicks} />
+    </div>
+  )
+
 };
 
 export default App;
