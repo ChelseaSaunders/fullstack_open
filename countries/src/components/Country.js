fb51c20d3c7ac73290aa8b83bbcbe1e00b048885
Chelsea Saunders <c_chilcoat@msn.com>
2022-11-15 04:40:38 -0800

    finished exercise 2.14 but didnt bother with weather icon

--- countries/src/components/Country.js
+++ countries/src/components/Country.js
@@ -1,6 +1,26 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+
 const Country = (props) => {
   const api_key = process.env.REACT_APP_API_KEY
+  const [weather, setWeather] = useState('');
+  const [temperature, setTemperature] = useState();
+
   const country = props.country[0];
+  const lat = country.capitalInfo.latlng[0];
+  const lon = country.capitalInfo.latlng[1];
+
+  const hook = () => {
+    axios
+      .get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}&units=imperial`)
+      .then(response => {
+        setWeather(response.data.weather[0].description);
+        setTemperature(response.data.main.temp);
+      });
+  };
+
+  useEffect(hook);
 
   const generateLanguageList = () => {
     let langKeys = Object.keys(country.languages);
@@ -18,7 +38,11 @@ const Country = (props) => {
       <ul>{generateLanguageList()}</ul>
       <img
         src={country.flags.png}
-        alt={`${country.name.official} flag`} />
+        alt={`${country.name.official} flag`}
+      />
+      <h1>Weather</h1>
+      <p>{weather}</p>
+      <p>{temperature}</p>
     </div>
   );
 };
