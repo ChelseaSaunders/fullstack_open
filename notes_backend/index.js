5cd0e2ce337dc59e6f58720988c7a246d5346dd2
Chelsea Saunders <c_chilcoat@msn.com>
2022-11-18 02:06:47 -0800

    got backend flyio server running

--- notes_backend/index.js
+++ notes_backend/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+app.use(cors());
 
 app.use(express.json());
 
@@ -76,7 +78,7 @@ app.post('/api/notes', (request, response) => {
   response.json(note);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`port: ${PORT}`);
+  console.log(`Server running on port ${PORT}`)
 });
