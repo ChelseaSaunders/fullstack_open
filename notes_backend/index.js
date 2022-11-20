5447cb417ec1eb4e868803c3aba00f4996d2daa0
Chelsea Saunders <c_chilcoat@msn.com>
2022-11-20 07:30:28 -0800

    keep forgetting to delete hard coded dummy notes; just did

--- notes_backend/index.js
+++ notes_backend/index.js
@@ -7,25 +7,6 @@ const Note = require('./models/note');
 app.use(cors());
 app.use(express.static('build'));
 app.use(express.json());
-//   {
-//     id: 1,
-//     content: "HTML is easy",
-//     date: "2022-05-30T17:30:31.098Z",
-//     important: true
-//   },
-//   {
-//     id: 2,
-//     content: "Browser can execute only Javascript",
-//     date: "2022-05-30T18:39:34.091Z",
-//     important: false
-//   },
-//   {
-//     id: 3,
-//     content: "GET and POST are the most important methods of HTTP protocol",
-//     date: "2022-05-30T19:20:14.298Z",
-//     important: true
-//   }
-// ];
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello</h1>');
@@ -103,7 +84,7 @@ const errorHandler = (error, request, response, next) => {
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT;
+const PORT = 8080;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 });
