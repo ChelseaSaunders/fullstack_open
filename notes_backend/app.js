f938a1a59ce8050e1c0d03df7c72d192f2812291
Chelsea Saunders <c_chilcoat@msn.com>
2022-11-22 02:51:05 -0800

    finished 4b

--- notes_backend/app.js
+++ notes_backend/app.js
@@ -1,13 +1,14 @@
-const config = require('./utils/config');
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const notesRouter = require('./controllers/notes');
-const middleware = require('./utils/middleware');
-const logger = require('./utils/logger');
-const mongoose = require('mongoose');
+const config = require('./utils/config')
+const express = require('express')
+require('express-async-errors')
+const app = express()
+const cors = require('cors')
+const notesRouter = require('./controllers/notes')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
 
-logger.info('connecting to', config.MONGODB_URI);
+logger.info('connecting to', config.MONGODB_URI)
 
 mongoose.connect(config.MONGODB_URI)
   .then(() => {
@@ -15,16 +16,16 @@ mongoose.connect(config.MONGODB_URI)
   })
   .catch((error) => {
     logger.error('error connecting to MongoDB:', error.message)
-  });
+  })
 
-app.use(cors());
-app.use(express.static('build'));
-app.use(express.json());
-app.use(middleware.requestLogger);
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+app.use(middleware.requestLogger)
 
-app.use('/api/notes', notesRouter);
+app.use('/api/notes', notesRouter)
 
-app.use(middleware.unknownEndpoint);
-app.use(middleware.errorHandler);
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
-module.exports = app;
+module.exports = app
