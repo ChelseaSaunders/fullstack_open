36f3392c1db9e84ed5df1b0cdc21f66fca07b952
Chelsea Saunders <c_chilcoat@msn.com>
2022-11-23 14:11:25 -0800

    backend working again, ._id propery of decodedToken was not working but username did instead, works now

--- notes_backend/controllers/notes.js
+++ notes_backend/controllers/notes.js
@@ -34,22 +34,23 @@ notesRouter.post('/', async (request, response) => {
   const body = request.body
   const token = getTokenFrom(request)
   const decodedToken = jwt.verify(token, process.env.SECRET)
+
   if (!decodedToken.id) {
     return response.status(401).json({ error: 'token missing or invalid' })
   }
 
-  const user = await User.findById(body.userId)
+  const user = await User.findOne({ username: decodedToken.username })
 
   const note = new Note({
     content: body.content,
     important: body.important === undefined ? false : body.important,
     date: new Date(),
-    // user: user._id
+    user: user._id
   })
 
   const savedNote = await note.save()
-  // user.notes = user.notes.concat(savedNote._id)
-  // await user.save()
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
 
   response.status(201).json(savedNote)
 })
--- notes_backend/models/note.js
+++ notes_backend/models/note.js
@@ -8,10 +8,10 @@ const noteSchema = new mongoose.Schema({
   },
   date: Date,
   important: Boolean,
-  // user: {
-  //   type: mongoose.Schema.Types.ObjectId,
-  //   ref: 'User'
-  // }
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
 })
 
 noteSchema.set('toJSON', {
