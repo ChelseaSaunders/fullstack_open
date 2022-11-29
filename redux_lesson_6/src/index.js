e549cefc4e3f232e4d01b3c3308a25ea50635223
ChelseaSaunders <c_chilcoat@msn.com>
2022-11-28 17:54:06 -0800

    finished through 6.3

--- redux_lesson_6/src/index.js
+++ redux_lesson_6/src/index.js
@@ -4,7 +4,8 @@ import { Provider } from 'react-redux'
 import { configureStore } from '@reduxjs/toolkit'
 import App from './App'
 
-import noteReducer from './reducers/noteReducer'
+import noteService from './services/notes'
+import noteReducer, { setNotes } from './reducers/noteReducer'
 import filterReducer from './reducers/filterReducer'
 
 const store = configureStore({
