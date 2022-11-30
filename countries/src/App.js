90e029bb60b325ae1725b67b9b95a21305f06989
ChelseaSaunders <c_chilcoat@msn.com>
2022-11-30 13:48:45 -0800

    error in app file just fixed; worknig on updating withl esson 7

--- countries/src/App.js
+++ countries/src/App.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-import Search from './components/Search';
-import CountryList from '.components/CountryList';
+import Search from './components/Search'
+import CountryList from './components/CountryList'
 
 const App = () => {
   const [countries, setCountries] = useState([]);
