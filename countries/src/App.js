146dc2e45b0b70374756f7727a4966afb0c53756
ChelseaSaunders <c_chilcoat@msn.com>
2022-11-30 16:47:04 -0800

    finished exercise 7.7

--- countries/src/App.js
+++ countries/src/App.js
@@ -8,31 +8,46 @@ const App = () => {
   const [countries, setCountries] = useState([]);
   const [searchValue, setSearchValue] = useState('');
 
-  const hook = () => {
-    axios
-      .get('https://restcountries.com/v3.1/all')
-      .then(response => {
-        setCountries(response.data);
-      });
-  };
+  // const hook = () => {
+  //   axios
+  //     .get('https://restcountries.com/v3.1/all')
+  //     .then(response => {
+  //       setCountries(response.data);
+  //     });
+  // };
+
+  const useCountry = () => {
+    if (searchValue !== '') {
+      axios
+        .get(`https://restcountries.com/v3.1/name/${searchValue}`)
+        .then(response => {
+          setCountries(response.data)
+        })
+        .catch((error) => {
+          console.log(error)
+          alert('Search did not retrieve any matches, try again.')
+        })
+    }
+  }
 
   const handleSearch = event => setSearchValue(event.target.value);
 
   const handleButtonClick = event => setSearchValue(event.target.name);
 
-  const matchingCountries = searchValue === '' ? [] : countries
-    .filter((country) => {
-      let regEx = new RegExp(searchValue, 'gi');
-      return country.name.common.match(regEx);
-    });
+  // const matchingCountries = searchValue === '' ? [] : countries
+  //   .filter((country) => {
+  //     let regEx = new RegExp(searchValue, 'gi');
+  //     return country.name.common.match(regEx);
+  //   });
 
-  useEffect(hook, []);
+  // useEffect(hook, []);
+  useEffect(useCountry, [searchValue])
 
   return (
     <React.Fragment>
       <Search handleSearch={handleSearch} />
       <CountryList
-        countries={matchingCountries}
+        countries={countries}
         handleButtonClick={handleButtonClick}
       />
     </React.Fragment>
